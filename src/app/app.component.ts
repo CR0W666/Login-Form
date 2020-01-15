ced9b71fb17b8b8308bda287c9f677238718362e
CR0W666 <lukas.petracek@educanet.cz>
2020-01-15 12:50:50 +0100

    fin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'LoginForm';
+  private username = "";
+  private password = "";
+  private readonly correctUsername = "admin";
+  private readonly correctPassword = "admin";
+  private logged = false;
+
+  buttonClicked() {
+    if(this.username === this.correctUsername && this.password === this.correctPassword) {
+      this.logged = true;
+      alert("Welcome back " + this.correctUsername);
+    } else {
+      alert("Wrong input credentials.");
+    }
+  }
+  
 }
